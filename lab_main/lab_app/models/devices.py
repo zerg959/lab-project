c1d60792837418f4eb7941fae9019af7413cab4c
zerg959 <zerg959@yandex.ru>
2025-02-04 17:53:37 +0300

    model: sensor.structure

--- lab_main/lab_app/models/devices.py
+++ lab_main/lab_app/models/devices.py
@@ -10,6 +10,15 @@ class Device(Base):
     """
     Abstract class.
     Parent Device model for Sensor and Regulator models.
+    to create Devices of different types: Sensors, Regulators etc.
+    Attributes:
+    id (plymorphic id from children class): unique id of the device-type object.
+    zone_id(int): ID of the linked Zone.
+    description (str): Device description. Default="device".
+    is_active (bool): Device status (on/off). Default value = "False".
+    is_outdoor (bool): Device loctation flag - (indoor/outdoor). Default value = "False"
+    device_type (str): Type of the Device (for ex.: sensor, regulator). Default value = DEVICE_TYPE_SENSOR.
+
     """
     __abstract__ = True
     zone_id = Column(Integer, ForeignKey('zones.id'), nullable=True)
@@ -18,8 +27,9 @@ class Device(Base):
     is_active = Column(Boolean, default=False, nullable=False)
     is_outdoor = Column(Boolean, default=False, nullable=False)
     device_type = Column(String,
-                         CheckConstraint(f'device_type IN ("{DEVICE_TYPE_SENSOR}", "{DEVICE_TYPE_REGULATOR}")'),
-        nullable=False,
+                         CheckConstraint(f'device_type IN ("{DEVICE_TYPE_SENSOR}", "{DEVICE_TYPE_REGULATOR}")'), 
+                         default=DEVICE_TYPE_SENSOR, 
+                         nullable=False,
     )
 
 
@@ -27,6 +37,7 @@ class Device(Base):
 class Sensor(Device):
     """
     Sensor class.
+    Sibling of the Device class.
     Use for integration of measurement devices: co2-, t-, humidity- sensors.
     """
     __tablename__ = "sensors"
@@ -36,4 +47,5 @@ class Sensor(Device):
     }
     current_sensor_param = Column(Float, defaulr='N/A', nullable=True)
 
-
+    def __repr__(self):
+        return f"Sensor {self.id}: {self.description}"
