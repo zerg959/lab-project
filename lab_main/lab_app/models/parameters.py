4ba6ea08d495f128a731751d6a7d6d838516b990
zerg959 <zerg959@yandex.ru>
2025-03-09 15:28:01 +0300

    devices docstr

--- lab_main/lab_app/models/parameters.py
+++ lab_main/lab_app/models/parameters.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
+from typing import List
 
 # from datetime import datetime, timezone
 from .base import Base
@@ -7,37 +8,31 @@ from .base import Base
 
 class Parameter(Base):
     """
-    Parameter class.
+    Represents a parameter that can be measured or managed by sensors or
+    regulators, respectively.
+
+    Attributes:
+        id (int):
+        Unique identifier for the parameter.
+        parameter_name (Optional[str]):
+        Name of the parameter (e.g., "temperature", "humidity").
+        parameter_unit (Optional[str]):
+        Unit of measurement for the parameter (e.g., "°C", "%"). Default=None.
+        description (str): Description of the parameter. Default="parameter".
+        sensors (List[:class:`Sensor`]):
+        List of sensors associated with this parameter.
     """
 
     __tablename__ = "parameters"
-    """
-    tablename: DB-table name.
-    """
+
     id = Column(Integer, primary_key=True)
-    """
-    id (int): Parameter-object ID.
-    """
     parameter_name = Column(String, nullable=True)
-    """
-    parameter_name (str): name of the parameter
-    (for ex: temperature, humidity). Default: None.
-    """
     parameter_unit = Column(String, nullable=True)
-    """
-    parameter_unit (str): parameter unit. Default: None.
-    """
     description = Column(String, default="parameter")
-    """
-    description (str): parameter description. Default: None.
-    """
     sensors = relationship(
         "Sensor",
         back_populates="parameter",
     )
-    """
-    sensors (list): list of linked sensors.
-    """
     # timestamp = Column(DateTime,
     # default=datetime.datetime.now(timezone.utc)) # Date and time
 
