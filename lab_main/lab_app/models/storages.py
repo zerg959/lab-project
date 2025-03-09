c42bc16c90f0ad6801320be63c96183ee4ec4915
zerg959 <zerg959@yandex.ru>
2025-03-09 15:32:39 +0300

    storage docstr

--- lab_main/lab_app/models/storages.py
+++ lab_main/lab_app/models/storages.py
@@ -4,43 +4,38 @@ from sqlalchemy import (
     Integer,
     String,
 )
+from typing import List
 from .associations import association_table
 from .base import Base
 
 
 class Storage(Base):
     """
-    Storage model:
-    to create Storage with Zones.
-    One Storage can be managed by multiply Users.
-    Attributes:
-    id (int): Unique storage ID.
-    description (str): Storage description.
-    users (list): Linked users.
-    zones (list): Store zones.
+      Represents a storage area with associated zones and users.
 
-    """
+      A storage area can contain multiple zones with different environmental
+      conditions.
+      Multiple users can be authorized to manage a storage area.
 
+      Attributes:
+          id (int):
+          Unique identifier for the storage area.
+          description (Optional[str]):
+          Description of the storage area.
+          users (List[:class:`User`]):
+          List of users who have access to manage this storage area.
+          The relationship is many-to-many via the `association_table`.
+          zones (List[:class:`Zone`]):
+          List of zones contained within this storage area.
+      """
     __tablename__ = "storages"
     id = Column(Integer, primary_key=True, nullable=False)
-    """
-    id (int): Storage ID in DB.
-    """
     description = Column(String, nullable=True)
-    """
-    description (str): Storage description.
-    """
     users = relationship("User",
-                         secondary=association_table,
-                         back_populates="storages"
-                         )
-    """
-    users (list): Users who can manage Storage.
-    """
+                        secondary=association_table,
+                        back_populates="storages"
+                        )
     zones = relationship("Zone", back_populates="storage")
-    """
-    zones (list): Zones for different conditions added in Storage.
-    """
 
     def __repr__(self):
-        return f"Storage {self.id}: {self.description}"
+        return f"Storage id={self.id}: {self.description}"
